72cb8de801ded6b0c7ed48ae208f2e0c071c27d3
Thibault Monnier <thibaultmonni@gmail.com>
2024-10-12 18:39:48 +0200

    Init project with ts, vite, react and styled-components

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+    base: '/',
+    plugins: [react()],
+    server: {
+        open: true,
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            src: path.resolve(__dirname, './src'),
+        },
+    },
+    optimizeDeps: {
+        force: true,
+    },
+    assetsInclude: ['**/*.md'], // Treat .md files as assets
+    build: {
+        outDir: 'build',
+        minify: 'esbuild',
+        brotliSize: true,
+    },
+})
