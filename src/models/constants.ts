19f81757f274f90a93b8e3992f21e9084f760782
Thibault-Monnier <thibaultmonni@gmail.com>
2025-03-01 14:40:34 +0100

    Use real robot size + update piloting algorithm

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -2,7 +2,7 @@ export const stepDurationMs = 2 // Takes a long time to communicate with the ser
 export const encoderImpulsesPerWheelTurn = 500 * 36 * 4
 
 export const controlledRobotWheelDiameter = 65 // millimeters
-export const controlledRobotWidth = 350 // millimeters
-export const controlledRobotHeight = 350 // millimeters
+export const controlledRobotWidth = 385 // millimeters
+export const controlledRobotHeight = 385 // millimeters
 export const controlledRobotWheelsGap = 330 // millimeters
 export const controlledRobotMaxSpeed = 500 // millimeters per second
